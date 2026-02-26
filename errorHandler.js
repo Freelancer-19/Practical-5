03b71ffb33a42f04ceb35b46a7594101b2424c22
Yash <142044524+Freelancer-19@users.noreply.github.com>
2026-02-26 09:22:51 +0530

    Add files via upload

---
+++ errorHandler.js
@@ -0,0 +1,11 @@
+// middleware/errorHandler.js
+const errorHandler = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+    stack: process.env.NODE_ENV === 'development' ? err.stack : {}
+  });
+};
+
+module.exports = errorHandler;
